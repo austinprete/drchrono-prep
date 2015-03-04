ed5d0cf6f8f6003ba5ac6bfcab157528cb0ccf74
austinprete <acp2142@gmail.com>
2015-03-04 00:52:28 -0800

    Fixed templates

--- drchrono_prep/settings.py
+++ drchrono_prep/settings.py
@@ -91,7 +91,7 @@ TEMPLATE_DIRS = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'acp2142@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
